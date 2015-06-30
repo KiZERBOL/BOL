f4a22331a48aeaceab082f56fc4137682cfff49b
KiZERBOL <lizer008@gmail.com>
2015-06-30 18:28:56 +0200

    Update Ryze - Filled with Magic.lua

--- Ryze - Filled with Magic.lua
+++ Ryze - Filled with Magic.lua
@@ -1,1 +1,32 @@
+--[[Ryze - Filled with Magic by KiZER]]
 
+
+
+if myHero.charName ~= "Ryze" then return end
+
+_G.AUTOUPDATE = true -- Change to "false" to disable auto updates!
+
+local version = "0.01"
+local author = "KiZER"
+local UPDATE_HOST = "raw.github.com"
+local UPDATE_PATH = "/KiZERBOL/BOL/Ryze - Filled with Magic.lua".."?rand="..math.random(1,10000)
+local UPDATE_FILE_PATH = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
+local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
+function AutoupdaterMsg(msg) print("<font color=\"#FF0000\"><b>Ryze - Filled with Magic:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
+if _G.AUTOUPDATE then
+	local ServerData = GetWebResult(UPDATE_HOST, "KiZERBOL/BOL/Ryze - Filled with Magic.version")
+	if ServerData then
+		ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
+		if ServerVersion then
+			if tonumber(version) < ServerVersion then
+				AutoupdaterMsg("New version available "..ServerVersion)
+				AutoupdaterMsg("Updating, please don't press F9")
+				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
+			else
+				AutoupdaterMsg("You have got the latest version ("..ServerVersion..")")
+			end
+		end
+	else
+		AutoupdaterMsg("Error downloading version info")
+	end
+end
